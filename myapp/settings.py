74f97a483f0d892ac5970b597fb5a8155e016435
M-Bahy <Homelander.SolderBoy@gmail.com>
2023-07-12 14:09:30 +0300

    containers communicate with each other

--- myapp/settings.py
+++ myapp/settings.py
@@ -81,22 +81,22 @@ ROOT_URLCONF = "myapp.urls"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "db1",
-#         "USER": "bahy",
-#         "PASSWORD": "mnk3mnk3",
-#         # "Host": "172.17.0.2",
-#         "PORT": 5432,
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db1",
+        "USER": "bahy",
+        "PASSWORD": "mnk3mnk3",
+        "Host": "172.17.0.2",
+        "PORT": 5432,
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
